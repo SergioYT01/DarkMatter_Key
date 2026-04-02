a529c93a7ec74bc3c1ac1868e764425880ca9684
SergioYT01 <sergio0102yt@gmail.com>
2026-04-02 18:37:05 -0500

    Update DarkMatter_key.lua

--- DarkMatter_key.lua
+++ DarkMatter_key.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/darkmatter-oficial/Panel-DarkMatter/refs/heads/main/Key%20Sistema.lua?token=GHSAT0AAAAAADZPUVPLAVMBFXVRJOGXZSXI2OO7MHQ"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/darkmatter-oficial/Panel-DarkMatter/refs/heads/main/Key%20Sistema.lua"))()
